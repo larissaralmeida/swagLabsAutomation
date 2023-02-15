be8081d5e80c62c7ed43e32457f96d542e175b13
Larissa Rodrigues <48097023+larissaralmeida@users.noreply.github.com>
2023-02-15 13:59:48 -0300

    Update driver.java

--- src/main/java/org/example/driver.java
+++ src/main/java/org/example/driver.java
@@ -8,7 +8,7 @@ public class driver {
 
 
     public WebDriver WebDriver(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\laris\\IdeaProjects\\curso1\\ChromeDriver\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "..chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://www.saucedemo.com");
